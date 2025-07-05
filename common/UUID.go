4abda3baf6c30a38713106b4762722b760d9035f
Nikita <Nik-Mir-Vas@yandex.ru>
2025-07-05 14:43:35 +0300

    Modified UUID

--- common/UUID.go
+++ common/UUID.go
@@ -1,3 +1,21 @@
 package common
 
+import (
+	"encoding/base64"
+)
+
 type UUID [16]byte
+
+func (u UUID) ToString() string {
+	return base64.RawStdEncoding.EncodeToString(u[:]);
+}
+
+func getUUIDFromString(s string) (UUID, error) {
+	var u UUID
+	data, err := base64.RawStdEncoding.DecodeString(s)
+	if err != nil {
+		return u, err
+	}
+	copy(u[:], data)
+	return u, nil
+}
