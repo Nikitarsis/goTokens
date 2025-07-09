03756e29193d1df5133cdb2fae362b22df1d5dab
Nikita <Nik-Mir-Vas@yandex.ru>
2025-07-09 13:19:31 +0300

    Inmemory changes

--- repository/inmemory/IpRepositoryInMemory.go
+++ repository/inmemory/IpRepositoryInMemory.go
@@ -2,22 +2,57 @@ package inmemory
 
 import (
 	"fmt"
+	"sync"
 
 	co "github.com/Nikitarsis/goTokens/common"
+	inter "github.com/Nikitarsis/goTokens/repository/interfaces"
 )
 
 // In-Memory репозиторий IP, отправляющий данные в консоль.
 //
 // Да, это не репозиторий на самом деле, но это простая реализация для тестирования.
 type IpRepositoryInMemory struct {
+	savePorts bool
+	innerMap  *co.SafeMap[co.UUID, *co.SafeSet[string]]
+	mutex     sync.Mutex
 }
 
 // CreateInMemoryIPRepository создает новый экземпляр IpRepositoryInMemory.
-func CreateInMemoryIPRepository() co.IIpRepository {
-	return &IpRepositoryInMemory{}
+func CreateInMemoryIPRepository(savePorts bool) inter.IIpRepository {
+	return &IpRepositoryInMemory{
+		savePorts: savePorts,
+		innerMap:  co.CreateSafeMap[co.UUID, *co.SafeSet[string]](),
+	}
 }
 
-// TraceIp отслеживает IP-адреса.
-func (r *IpRepositoryInMemory) TraceIp(data co.DataIP) {
-	fmt.Printf("KeyId: %s, UserId: %s, FromPort: %d, IP: %s\n", data.KeyId.ToString(), data.UserId.ToString(), data.Port, data.IP.String())
+func (ir *IpRepositoryInMemory)	SaveIp(kid co.UUID, ip co.DataIP) error {
+	ir.mutex.Lock()
+	defer ir.mutex.Unlock()
+
+	data, ok := ir.innerMap.Load(kid)
+	if !ok {
+		data = co.CreateSafeSet[string]()
+	}
+	if (ir.savePorts) {
+		data.Store(fmt.Sprintf("%s:%d", ip.IP.String(), ip.Port))
+	}
+	data.Store(ip.IP.String())
+	ir.innerMap.Store(kid, data)
+	return nil
+}
+
+func (ir *IpRepositoryInMemory)	CheckIp(kid co.UUID, ip co.DataIP) bool {
+	ir.mutex.Lock()
+	defer ir.mutex.Unlock()
+
+	storedIp, ok := ir.innerMap.Load(kid)
+	if !ok {
+		return false
+	}
+	if ir.savePorts {
+		check := storedIp.Load(fmt.Sprintf("%s:%d", ip.IP.String(), ip.Port))
+		return check
+	}
+	check := storedIp.Load(ip.IP.String())
+	return check
 }
--- repository/inmemory/UserAgentRepositoryInMemory.go
+++ repository/inmemory/UserAgentRepositoryInMemory.go
@@ -6,25 +6,25 @@ import (
 
 // In-Memory репозиторий User-Agent
 type UserAgentRepositoryInMemory struct {
-	userAgents *co.SafeMap[co.UUID, string]
+	userAgents *co.SafeMap[co.UUID, co.UserAgentData]
 }
 
 // CreateInMemoryUserRepository создает новый экземпляр UserAgentRepositoryInMemory.
 func CreateInMemoryUserRepository() co.IUserAgentRepository {
-	safeMap := co.CreateSafeMap[co.UUID, string]()
+	safeMap := co.CreateSafeMap[co.UUID, co.UserAgentData]()
 	return &UserAgentRepositoryInMemory{
 		userAgents: safeMap,
 	}
 }
 
 // SaveUserAgent сохраняет User-Agent в репозитории.
-func (r *UserAgentRepositoryInMemory) SaveUserAgent(kid co.UUID, userAgent string) error {
+func (r *UserAgentRepositoryInMemory) SaveUserAgent(kid co.UUID, userAgent co.UserAgentData) error {
 	r.userAgents.Store(kid, userAgent)
 	return nil
 }
 
 // CheckUserAgent проверяет User-Agent в репозитории.
-func (r *UserAgentRepositoryInMemory) CheckUserAgent(kid co.UUID, userAgent string) bool {
+func (r *UserAgentRepositoryInMemory) CheckUserAgent(kid co.UUID, userAgent co.UserAgentData) bool {
 	ua, ok := r.userAgents.Load(kid)
 	if !ok {
 		return false
